3f9f792eb049acf019e911d710413046d669508a
farrux-torayev <farruxtorayev590@gmail.com>
2024-11-22 21:54:28 +0500

    info

---
+++ js/index.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        tablet: "682px",
+        desktop: "1170px",
+      },
+      padding: {
+        DEFAULT: "30px",
+        tablet: "0",
+      },
+    },
+    screens: {
+      tablet: "780px",
+      desktop: "1280px",
+    },
+  },
+};
